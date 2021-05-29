70b8884cec5bcc45f30a84f76a6165a7e3ca2b56
sreeramachandrag <sreeramachandrag@localhost>
2021-05-29 16:16:25 +0530

    initial code

---
+++ account-project/src/test/java/com/sample/account/AccountProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sample.account;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
